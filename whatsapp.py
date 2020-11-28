1ce92bb220f5170c9f600dc105c2d0b6344f4147
Aakarsh B <aakarshbiju@gmail.com>
2020-11-28 22:25:17 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -6,7 +6,7 @@ driver.get('https://web.whatsapp.com')
 
 input('scan and press a key: ')
 
-booty = driver.find_element_by_css_selector('span[title="Neeraj Ipe YP"]')
+booty = driver.find_element_by_css_selector('span[title="CHANGE THIS"]')
 booty.click()
 
 i=0
@@ -15,10 +15,6 @@ n=50
 while i<n:
     i+=1
     textarea = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    textarea.send_keys('I also made a whatsapp bot btw. Just flexing :P')
+    textarea.send_keys('Your Unique Message')
     sendbutton = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button/span')
     sendbutton.click()
-textarea = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-textarea.send_keys('Repetetive messages(n count), timed messages etc etc')
-sendbutton = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button/span')
-sendbutton.click()
